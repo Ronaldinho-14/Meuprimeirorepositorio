938123bb6328d0b3e6f92a71441856751b7aee16
Ronaldinho-14 <ronaldo.zancheta@escola.pr.gov.br>
2024-11-05 11:22:13 -0300

    Atualizei o arquivo istyle

--- style.cs
+++ style.cs
@@ -4,3 +4,18 @@ body{
     background: black;
 
 }
+
+header {
+    border: solid 2px white;
+    pading: 20px;
+    fonte-size: 32px;
+    color: white;
+}
+
+section {
+    blackground: rgb(184, 156, 213);
+    padding-bottom: 80px;
+    display: flex; 
+    jusify-content: center;
+
+}
